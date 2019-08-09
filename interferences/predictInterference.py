8690435b54bc67d103f8c29caaaf421ac059963e
Alexandre Borrel <alexandre.borrel@nih.gov>
2019-08-09 10:21:14 -0400

    Add interferences

---
+++ interferences/predictInterference.py
@@ -0,0 +1,99 @@
+from os import listdir, system, remove, path
+from numpy import mean, std
+
+from .toolbox import loadMatrixToDict, formatModelName
+
+
+PMODEL = "/home/aborrel/django_server/django_server/static/interferences/models/"
+
+
+class Predict:
+    def __init__(self, lmodels, prSession):
+
+        self.lmodels = lmodels
+        self.pdes2D = prSession + "2D.csv"
+        self.pOpera = prSession + "OPERA.csv"
+        self.prSession = prSession
+        self.err = 0
+
+
+    def predictAll(self):
+
+
+        dout = {}
+        for name_model in self.lmodels:
+            dout[name_model] = {}
+            prmodel = PMODEL + name_model + "/"
+            lRmodels = listdir(prmodel)
+            for Rmodel in lRmodels:
+                presult = self.prSession + "temp.txt"
+                self.predictRmodel(prmodel + Rmodel, presult)
+                dout[name_model][Rmodel.split(".")[0]] = loadMatrixToDict(presult, sep = ",")
+                if path.exists(self.prSession + "temp.txt"):
+                    remove(self.prSession + "temp.txt")
+        self.resultPred = dout
+
+
+    def predictRmodel(self, pmodel, pout):
+
+
+        cmd = "/home/aborrel/django_server/django_server/interferences/Rscripts/predictfromModel.R %s %s %s %s" % (self.pdes2D, self.pOpera, pmodel, pout)
+        print(cmd)
+        system(cmd)
+
+
+    def processResult(self):
+
+        if not "resultPred" in self.__dict__:
+            #print("Compute prediction")
+            self.err = 1
+            return "ERROR"
+
+        #print(self.resultPred)
+
+        dresult = {}
+        pprect = self.prSession + "predict.csv"
+
+        for modelInt in self.resultPred.keys():
+            for modelPart in self.resultPred[modelInt].keys():
+                for chemIn in self.resultPred[modelInt][modelPart].keys():
+                    chemID = self.resultPred[modelInt][modelPart][chemIn]["ID"]
+                    if not chemID in dresult.keys():
+                        dresult[chemID] = {}
+                    if not modelInt in dresult[chemID].keys():
+                        dresult[chemID][modelInt] = []
+                    if not self.resultPred[modelInt][modelPart][chemIn]["pred"] == "NA":
+                        dresult[chemID][modelInt].append(float(self.resultPred[modelInt][modelPart][chemIn]["pred"]))
+
+        # add SMILES
+        d2D = loadMatrixToDict(self.pdes2D)
+        for chem in dresult.keys():
+            for model in dresult[chem].keys():
+                M = mean(dresult[chem][model])
+                SD = std(dresult[chem][model])
+                dresult[chem][model] = {}
+                dresult[chem][model]["M"] = round(M, ndigits=2)
+                dresult[chem][model]["SD"] = round(SD, ndigits=2)
+            dresult[chem]["SMILES"] = d2D[chem]["SMILES"]
+
+
+
+
+
+        fpred = open(pprect, "w")
+        fpred.write("ID\tSMILES\t%s\n"%("\t".join(["M-" + str(formatModelName(i)) + "\tSD-" + str(formatModelName(i)) for i in self.lmodels])))
+        for chem in dresult.keys():
+            fpred.write("%s\t%s\t%s\n"%(chem, dresult[chem]["SMILES"], "\t".join([str(dresult[chem][i]["M"]) + "\t" +
+                                                     str(dresult[chem][i]["SD"])
+                                                     for i in self.lmodels])))
+            for model in self.lmodels:
+                dresult[chem][formatModelName(model)] = dresult[chem][model]
+                del dresult[chem][model]
+
+        fpred.close()
+
+
+
+
+
+        return dresult
---
+++ interferences/views.py
@@ -0,0 +1,169 @@
+from django.shortcuts import render
+from django.conf.urls import url
+
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from random import randint
+from os import path
+import json
+
+from .forms import uploadList, UploadChemList, QSARModelChoice
+from .toolbox import createFolder, loadMatrixToDict
+from .content import formatSMILES
+from .predictInterference import Predict
+
+
+
+
+def index(request):
+    if not "name_session" in request.session.keys():
+        a = randint(0, 1000000)
+        request.session.get("name_session", a)
+        request.session["name_session"] = a
+
+    print(request.session.get("name_session"))
+    return render(request, 'interferences/index.html', {
+    })
+
+
+
+def resources(request):
+    return render(request, 'interferences/resources.html', {
+    })
+
+
+
+
+def uploadSMILES(request):
+
+    name_session = request.session.get("name_session")
+    print(name_session)
+
+    if request.method == 'GET':
+        form_smiles = UploadChemList()
+    else:
+        form_smiles = UploadChemList(request.POST)
+
+    formUpload = uploadList(request.POST, request.FILES)
+
+
+    # If data is valid, proceeds to create a new post and redirect the user
+    if form_smiles.is_valid() == True:
+
+        content = form_smiles.cleaned_data["content"]
+        content = content.replace("\r", "")
+        content = content.split("\n")
+        content = list(dict.fromkeys(content))
+        if len(content) > 100:
+            return render(request, 'interferences/uploadSMILES.html', {"form_smiles":form_smiles,
+                                                           "from_upload": formUpload, "ErrorLine": "1"})
+
+        prSession = "/home/aborrel/django_server/django_server/interferences/temp/" + str(name_session) + "/"
+        createFolder(prSession, 1)
+
+        cinput = formatSMILES(content, prSession)
+        cinput.prepListSMILES()
+
+        return render(request, 'interferences/cleanSMILES.html',
+                          {"dSMILESIN": cinput.dclean["IN"], "ERRORSmiles": str(cinput.err),
+                           "dSMILESOUT": cinput.dclean["OUT"]})
+
+
+    elif formUpload.is_valid() == True:
+
+        prSession = "/home/aborrel/django_server/django_server/interferences/temp/" + str(name_session) + "/"
+        createFolder(prSession, 1)
+
+        pfileserver = prSession + "upload.txt"
+        with open(pfileserver, 'wb+') as destination:
+            for chunk in formUpload.files["docfile"].chunks():
+                destination.write(chunk)
+        destination.close()
+
+        filin = open(pfileserver, "r")
+        try:
+            content = filin.read()
+            filin.close()
+
+            lsmiles = content.split("\n")
+            lsmiles = list(dict.fromkeys(lsmiles))
+            if len(lsmiles) > 100:
+                return render(request, 'interferences/uploadSMILES.html', {"form_smiles":form_smiles,
+                                                           "from_upload": formUpload, "ErrorFile": "1"})
+            cinput = formatSMILES(lsmiles, prSession)
+            cinput.prepListSMILES()
+
+            return render(request, 'interferences/cleanSMILES.html',
+                          {"dSMILESIN": cinput.dclean["IN"], "ERRORSmiles": str(cinput.err),
+                           "dSMILESOUT": cinput.dclean["OUT"]})
+
+        except:
+            filin.close()
+            return render(request, 'chemmaps/uploadSMILES.html', {"form_smiles":form_smiles,
+                                                           "from_upload": formUpload, "ErrorFile": "1"})
+
+
+    return render(request, 'interferences/uploadSMILES.html', {"form_smiles":form_smiles,
+                                                           "from_upload": formUpload, "ErrorLine": "0"})
+
+
+
+
+def computeDesc(request):
+
+    name_session = request.session.get("name_session")
+    print(name_session)
+
+    # open file with
+    prsession = "/home/aborrel/django_server/django_server/interferences/temp/" + str(name_session) + "/"
+    dSMI = loadMatrixToDict(prsession + "smiClean.csv", sep="\t")
+
+    cinput = formatSMILES(dSMI, prsession)
+    lfileDesc = cinput.computeDesc()
+
+    if cinput.err == 1:
+        return render(request, 'interferences/computeDESC.html', {"map": map, "dSMILESIN": cinput.dclean["IN"],
+                                                                  "ddesc": cinput.ddesc,
+                                                                  "dSMILESOUT": cinput.dclean["OUT"],
+                                                                  "ErrorDesc": "1"})
+    # form for descriptors
+    if request.method == 'GET':
+        formModel = QSARModelChoice()
+        return render(request, 'interferences/computeDESC.html', {"map": map, "dSMILESIN": cinput.dclean["IN"],
+                                                                  "dSMILESOUT": cinput.dclean["OUT"],
+                                                                  "ddesc": cinput.ddesc,
+                                                                  "form_model": formModel, "Error": "0"})
+    else:
+        formModel = QSARModelChoice(request.POST)
+
+    # run map with new chem
+    if formModel.is_valid() == True:
+        ldescMap = formModel.clean_desc()
+
+        cPred = Predict(ldescMap, prsession)
+        cPred.predictAll()
+        dpred = cPred.processResult()
+        #print(dpred)
+        dpredJS = json.dumps(dpred)
+
+        return render(request, 'interferences/results.html', {"dpred":dpredJS})
+
+    else:
+        return render(request, 'interferences/computeDESC.html', {"map": map, "dSMILESIN": cinput.dclean["IN"],
+                                                                 "dSMILESOUT": cinput.dclean["OUT"],
+                                                                 "ddesc": cinput.ddesc,
+                                                                 "form_model": formModel, "Error": "1"})
+
+
+
+def download(request, name):
+
+    name_session = request.session.get("name_session")
+    file_path = "/home/aborrel/django_server/django_server/interferences/temp/" + str(name_session) + "/" + name + ".csv"
+    if path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + path.basename(file_path)
+            return response
+    raise Http404
+
